49c882bbbbf9fc4852d0a16010630f5142bff625
Shelby Armitage <shelbyarmitage@outlook.com>
2023-11-20 21:11:21 -0400

    fixed splash image

--- Assignment3/script.js
+++ Assignment3/script.js
@@ -7,3 +7,29 @@ document.addEventListener('DOMContentLoaded', function () {
         hiddenContent.classList.toggle('hidden');
     });
 });
+
+
+function adjustImageSize() {
+    const logoImage = document.getElementById('logo');
+    const mainImage = document.getElementById('mainImage');
+    const splashImage = document.getElementById('splashImage');
+
+    const windowWidth = window.innerWidth;
+
+    // Adjust image size based on window width
+    if (windowWidth < 600) {
+        logoImage.style.width = '50px';
+        mainImage.style.width = '80%';
+        splashImage.style.width = '80%';
+    } else {
+        logoImage.style.width = '100px';
+        mainImage.style.width = '100%';
+        splashImage.style.width = '90%';
+    }
+}
+
+// Attach the adjustImageSize function to the window resize event
+window.addEventListener('resize', adjustImageSize);
+
+// Call the function initially to set the initial image sizes
+adjustImageSize();
