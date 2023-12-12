6b85ccae969a06b2426a04393f28c71cf43f50d2
W0491348 <144266165+W0491348@users.noreply.github.com>
2023-12-12 19:29:05 -0400

    move to github io

--- FinalAssignment/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenuButton = document.getElementById('mobile-menu');
-    const navList = document.querySelector('.nav-list');
-
-    mobileMenuButton.addEventListener('click', function () {
-        navList.classList.toggle('active');
-    });
-});
